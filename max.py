32021f01912e2c4aeea33edf7a25eecbb457bd31
Zhirao Zhang <jianye2020@outlook.com>
2021-10-23 10:09:22 +0800

    规范一下

--- max.py
+++ max.py
@@ -1,15 +1,19 @@
 #比较大小函数
 def max(a, b):
     if a >= b:
+        print("a的值更大")
         return a
     else:
+        print("b的值更大")
         return b
-
 if __name__ == '__main__' :
-    a = input("请输入a")
+    a = input("请输入a：")
     int(a)
     print('a的值为', a)
-    b=input('请输入b')
+    print('-------------')
+    b=input('请输入b；')
     int(b)
     print('b的值为', b)
+    print('-------------')
     print('最大值为', max(a, b))
+    print('-----End-----')
